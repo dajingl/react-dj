acee3616ebb1910c44d7d6d25456f1229b8a1f3d
dj <dajing61@foxmail.com>
2018-02-02 17:49:42 +0800

    按需加载# 引入 react-app-rewired 并修改 package.json

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const injectBabelPlugin = require('react-app-rewired');
+const { injectBabelPlugin } = require('react-app-rewired');
 module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: true }], config);
     return config;
